a83e07dd6d290d75b2bcd69dd3ac5f5ff52a4b55
anjana <146495563+AnjanaTinush@users.noreply.github.com>
2025-10-04 07:51:39 +0530

    change colors

--- app/src/main/java/com/example/ev_syatem/database/DatabaseHelper.kt
+++ app/src/main/java/com/example/ev_syatem/database/DatabaseHelper.kt
@@ -64,6 +64,7 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         db.execSQL("DROP TABLE IF EXISTS $TABLE_RESERVATION")
         db.execSQL("DROP TABLE IF EXISTS $TABLE_USER")
+
         onCreate(db)
     }
 }
